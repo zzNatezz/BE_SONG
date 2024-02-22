f0a070443477e55472b0a6868e66e76b1e4d850c
zzD3chzz <thcdzch@gmail.com>
2024-02-22 22:09:35 +0700

    complete delete song

--- modell/songModel.js
+++ modell/songModel.js
@@ -1,24 +1,28 @@
 import mongoose from "mongoose";
 
-const {Schema} = mongoose ;
+const { Schema } = mongoose;
 
-const songSchema = new Schema ({ 
-    title : {type: String, require : true},
-    author : {type: String, require : true},
-    view : Number,
-    image : [{
-        url:String,
-        publicId:String
-    }],
-    createdAt : {type: Date, default : Date.now},
-    isPublic : Boolean,
-    song : [{
-        url:String,
-        publicId:String
-    }],
-    updatedAt : Date
-})
+const songSchema = new Schema({
+  title: { type: String, require: true },
+  author: { type: String, require: true },
+  view: Number,
+  image: [
+    {
+      url: String,
+      publicId: String,
+    },
+  ],
+  createdAt: { type: Date, default: Date.now },
+  isPublic: Boolean,
+  song: [
+    {
+      url: String,
+      publicId: String,
+    },
+  ],
+  updatedAt: Date,
+});
 
-const songModel = mongoose.model('song', songSchema);
+const songModel = mongoose.model("song", songSchema);
 
-export {songModel};
+export { songModel };
