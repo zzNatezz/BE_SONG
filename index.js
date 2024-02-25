5ed56554f6225fcf6aed1f3896775723f6c11c96
vuduchiieu <vuduchiieu@gmail.com>
2024-02-25 20:35:36 +0700

    update

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ import express from "express";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import cors from "cors";
+import admin from "firebase-admin";
 import cookieParser from "cookie-parser";
 import authRoute from "./routes/authRoute.js";
 import userRoute from "./routes/userRoute.js";
 import songRoute from "./routes/songRoute.js";
+import serviceAccount from "./songs-cac58-firebase-adminsdk-u9fi1-aecf840a24.json" assert { type: "json" };
 
 dotenv.config();
 
@@ -18,6 +20,11 @@ const crosOptions = {
   optionsSuccessStatus: 204,
 };
 
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://songs-cac58.firebaseio.com",
+});
+
 sv.use(cors(crosOptions));
 sv.use(cookieParser());
 
@@ -27,7 +34,7 @@ sv.use(morgan("combined"));
 sv.use("/v1/songs", songRoute);
 sv.use("/v1/auth", authRoute);
 sv.use("/v1/user", userRoute);
-sv.use('/index', (req, res) => res.send(`ok`))
+sv.use("/", (req, res) => res.send(`ok`));
 
 mongoose
   .connect(process.env.MONGODB)
