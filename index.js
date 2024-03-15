f84a30fdc3359ab169c805dd19aa2dff6d8d4437
vuduchiieu <vuduchiieu@gmail.com>
2024-03-15 22:33:02 +0700

    cors

--- index.js
+++ index.js
@@ -15,6 +15,16 @@ const sv = express();
 
 sv.use(cors());
 
+sv.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 sv.use(cookieParser());
 
 sv.use(express.json());
